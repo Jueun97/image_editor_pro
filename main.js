bd7ae0a2980a327e5cb5385054c01ba907b21f14
Jueun <zxnm0901@naver.com>
2021-02-19 20:08:58 +0900

    js: text movement

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ const textBtn = document.querySelector('.option-text');
 const textContainer = document.querySelector('.board__text');
 let selectedTextBox = null;
 
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+let active = null;
+
 imageFile.addEventListener('change', () => {
     console.log("hello1");
     const reader = new FileReader();    
@@ -19,7 +24,9 @@ imageFile.addEventListener('change', () => {
     }) */
         imageBox.src = reader.result;
         menuBar.style.display = 'block';
-})
+    })
+    const textContainerX = imageBox.getBoundingClientRect().left;
+    textContainer.style.left = `${textContainerX}px`;
     reader.readAsDataURL(imageFile.files[0])
 })
 
@@ -31,14 +38,38 @@ textBtn.addEventListener('click', () => {
     textBox.style.left = '0';
 
     textContainer.append(textBox);
+
+    active = 'text';
 })
 
 textContainer.addEventListener('mousedown', (event) => {
     selectedTextBox = event.target;
-    console.log("down");
+    console.log("down", selectedTextBox);
 })
 textContainer.addEventListener('mouseup', (event) => {
     selectedTextBox = null;
     console.log("up");
 })
 
+canvas.addEventListener('mousedown', () => {
+    console.log('canvas down');
+});
+canvas.addEventListener('mousemove', (event) => {
+    let coordinateX = event.layerX;
+    let coordinateY = event.layerY;
+
+    switch (active) {
+        case 'draw': ;
+        case 'erase': ;
+        case 'text': {
+            if (selectedTextBox) {
+                selectedTextBox.style.transform = `translate(${coordinateX}px,${coordinateY}px)`;
+            }
+        };
+        case 'background': ;
+        default: ;
+    }
+});
+canvas.addEventListener('mouseup', () => {
+    console.log('canvas up');
+});
